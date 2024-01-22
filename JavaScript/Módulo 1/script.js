ac96041916a94e866c569b30e1b94ae98d81f9ba
vitoria-vieirac <114622524+vitoria-vieirac@users.noreply.github.com>
2024-01-22 16:44:39 -0300

    Primeiros Exercícios

---
+++ JavaScript/Módulo 1/script.js
@@ -0,0 +1,12 @@
+console.log('Hello Word!')
+
+const mediaNumbers = (2+4+6)/3
+
+console.log(mediaNumbers)
+
+const number1 = 7
+const number3 = 10
+
+const result = `A soma de ${number1} e ${number3} é igual a ${number1 + number3}`
+
+console.log(result)
