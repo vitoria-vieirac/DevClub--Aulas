9b666d2cb3f9bbfcf07919251f83416c8fb95c9d
vitoria-vieirac <114622524+vitoria-vieirac@users.noreply.github.com>
2024-01-29 13:58:57 -0300

    HTML do projeto

---
+++ JavaScript/Módulo Web/script.js
@@ -0,0 +1,12 @@
+const input = document.querySelector("#first-input")
+
+function cliqueiNoBotao() {
+    console.log(input.value)
+    
+}
+
+function digiteiNoInput() {
+    console.log(input.value)
+}
+
+digiteiNoInput()
